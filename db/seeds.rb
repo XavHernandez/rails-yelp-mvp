a8548a386afc3488747749e5e8158426630a4ec8
Xavier Hernandez <xhernan.martinez@gmail.com>
2021-05-12 12:28:12 +0200

    Seed and Controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: 'Macdo', category: 'belgian', address: '18 Blv des Burgers', phone_number: '+3369854322')
+Restaurant.create(name: 'Mazette', category: 'french', address: '5 Av Montparnasse', phone_number: '0698543784')
+Restaurant.create(name: 'Peppone', category: 'italian', address: '28 Av des Tauzins 33610 Cestas')
+Restaurant.create(name: 'Zob', category: 'chinese', address: '18 Covid Boulevard')
+Restaurant.create(name: 'Last', category: 'belgian', address: '116 Rue des Seeds', phone_number: '0798549615')
